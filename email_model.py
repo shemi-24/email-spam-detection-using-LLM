266cc39bb1171a7337a61a7b26d2dedba1029a07
shemeer <shemeerummer24@gmail.com>
2025-01-29 17:01:24 +0530

    more pip added and made debug false

--- email_model.py
+++ email_model.py
@@ -122,7 +122,8 @@ def categorize():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
 
 
 # #crct code
